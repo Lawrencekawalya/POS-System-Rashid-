c29d377bddc151696a8af5c41fcf524e85100126
Lawrencekawalya <kawalya.lawrence2016@gmail.com>
2026-02-27 22:59:46 +0300

    .

--- resources/views/layouts/auth/simple.blade.php
+++ resources/views/layouts/auth/simple.blade.php
@@ -4,7 +4,7 @@
         @include('partials.head')
         <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title ?? 'Login' }} | Scent of Elegance</title>
+    <title>{{ $title ?? 'Login' }} | AR Scents</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=cormorant-garamond:400,500,600,700|montserrat:300,400,500,600" rel="stylesheet" />
@@ -70,4 +70,28 @@
         @fluxScripts
     </body>
 </html>
+
+{{--<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    <head>
+        @include('partials.head')
+    </head>
+    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+        <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div class="flex w-full max-w-sm flex-col gap-2">
+                <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
+                    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
+                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                    </span>
+                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                </a>
+                <div class="flex flex-col gap-6">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+        @fluxScripts
+    </body>
+</html>--}}
+
 
